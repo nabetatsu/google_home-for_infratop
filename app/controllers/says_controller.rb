1ba0854720a441a415c009b99c15ec176e6e80f6
naberina <nabee.wata@gmail.com>
2018-05-13 18:34:01 +0900

    modify says_controller

--- app/controllers/says_controller.rb
+++ app/controllers/says_controller.rb
@@ -3,7 +3,7 @@ class SaysController < ApplicationController
   end
 
   def create
-	res = Net::HTTP.post_form(URI.parse('https://b8ee99df.ngrok.io/google-home-notifier'), {'text' => params[:text]})
-	redirect_to root_path
+		res = Net::HTTP.post_form(URI.parse('https://b8ee99df.ngrok.io/google-home-notifier'), {'text' => params[:text]})
+		redirect_to root_path
   end
 end
